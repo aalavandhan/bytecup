24dc834fb569b8f9f49d6ce504bb888673d7e533
Nithin Krishna <nithinkrishh@gmail.com>
2016-10-26 16:04:37 -0700

    updated run script

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
-python run.py ip/xaa &
-python run.py ip/xab &
-python run.py ip/xac &
-python run.py ip/xad &
-python run.py ip/xae &
-python run.py ip/xaf &
-python run.py ip/xag &
-python run.py ip/xah &
-python run.py ip/xai &
-python run.py ip/xaj &
-python run.py ip/xak &
-python run.py ip/xal &
-python run.py ip/xam &
-python run.py ip/xan &
-python run.py ip/xao &
+python run.py data/invited_info_train.txt ip/xaa &
+python run.py data/invited_info_train.txt ip/xab &
+python run.py data/invited_info_train.txt ip/xac &
+python run.py data/invited_info_train.txt ip/xad &
+python run.py data/invited_info_train.txt ip/xae &
+python run.py data/invited_info_train.txt ip/xaf &
+python run.py data/invited_info_train.txt ip/xag &
+python run.py data/invited_info_train.txt ip/xah &
+python run.py data/invited_info_train.txt ip/xai &
+python run.py data/invited_info_train.txt ip/xaj &
+python run.py data/invited_info_train.txt ip/xak &
+python run.py data/invited_info_train.txt ip/xal &
+python run.py data/invited_info_train.txt ip/xam &
+python run.py data/invited_info_train.txt ip/xan &
+python run.py data/invited_info_train.txt ip/xao &
 wait
 cat ip/xaa.results ip/xab.results ip/xac.results ip/xad.results ip/xae.results ip/xaf.results ip/xag.results ip/xah.results ip/xai.results ip/xaj.results ip/xak.results ip/xal.results ip/xam.results ip/xan.results ip/xao.results > results.csv
