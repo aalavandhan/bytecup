8c59f4c5b7e2b89a86f26219ec1376df1bbe6cf4
Nithin Krishna <nithinkrishh@gmail.com>
2016-10-26 13:39:46 -0700

    parallel run file

---
+++ run.sh
@@ -0,0 +1,17 @@
+python run.py ip/xaa &
+python run.py ip/xab &
+python run.py ip/xac &
+python run.py ip/xad &
+python run.py ip/xae &
+python run.py ip/xaf &
+python run.py ip/xag &
+python run.py ip/xah &
+python run.py ip/xai &
+python run.py ip/xaj &
+python run.py ip/xak &
+python run.py ip/xal &
+python run.py ip/xam &
+python run.py ip/xan &
+python run.py ip/xao &
+wait
+cat ip/xaa ip/xab ip/xac ip/xad ip/xae ip/xaf ip/xag ip/xah ip/xai ip/xaj ip/xak ip/xal ip/xam ip/xan ip/xao > results.csv
