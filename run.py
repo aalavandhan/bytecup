fixed kncf rating prediction formula

run.py
@@ -6,6 +6,9 @@ from scipy.stats import pearsonr
 
 import sys
 
+UNASKED = 0
+IGNORED = -0.001
+
 def pearsoncorr(x,y):
   return pearsonr(x, y)[ 0 ]
 
@@ -13,24 +16,22 @@ def featurize(user):
   v = np.repeat(0, NUMBER_OF_QUESTIONS)
 
   def update_vector(r):
-      v[ question_index[r['question_id']] ] = r['answered']
+    v[ question_index[r['question_id']] ] = r['answered']
 
   # Questions the user has answered
   train_info[train_info.user_id == user].apply(update_vector, axis=1)
 
-  # Normalized
-  # v = v - v.mean()
   return v
 
 # User-User CF
-def recommend(q, u, K=3):
-  qi = question_index[q]
+def recommend(question, user, K=3):
+  qi = question_index[ question]
 
   # active user
-  active_user = featurize(u)
+  active_user = featurize(user)
 
   # users who've be asked this question
-  users = train_info[train_info.question_id == q]
+  users = train_info[train_info.question_id == question]
   # users who've answered this question
   users = users[users.answered == 1]['user_id']
   user_vectors = map(featurize, users)
@@ -39,15 +40,15 @@ def recommend(q, u, K=3):
   top_k = sorted(user_vectors, key=lambda x: pearsoncorr(active_user, x) )[ :K ]
 
   # predicted rating
-  weighted_sum = reduce(lambda m, u: m + (u[qi] * pearsoncorr(active_user, u)), top_k, 0)
-  sum_of_weights = reduce(lambda m, u: m + u[qi], top_k, 0)
+  weighted_sum = reduce(lambda m, u: m + ((u[qi] - u.mean()) * pearsoncorr(active_user, u)), top_k, 0)
+  sum_of_weights = reduce(lambda m, u: m + pearsoncorr(active_user, u), top_k, 0)
 
   if sum_of_weights == 0 or np.isnan(weighted_sum) or np.isnan(sum_of_weights):
     recommended = 0
   else:
-    recommended = weighted_sum / sum_of_weights
+    recommended = active_user.mean() + weighted_sum / sum_of_weights
 
-  return max(0, recommended)
+  return min(1, recommended)
 
 
 ip = sys.argv[1]
@@ -75,6 +76,8 @@ question_index = { }
 for index, row in question_info.iterrows():
   question_index[ row['question_id'] ] = index
 
+# Setting value for ignored
+train_info.ix[train_info.answered == 0, 'answered'] = IGNORED
 
 recommendations = test_info.apply(lambda row: recommend(row['question_id'], row['user_id']), axis=1)
 